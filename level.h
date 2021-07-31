f9d4e18c5bedacc8aa971a3a5d1a4d1f9f08d7a7
ClearFate <luckyT320alter@gmail.com>
2021-07-31 03:30:44 +0200

    Updated level header file
    TODO - create state patern out of BrickType with loseHP() method that changes to previous brick. If brick is on 1hp then instead of change of state , put it nullptr.

--- level.h
+++ level.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "Tekstura.h"
+#include "BrickType.h"
+#include <vector>
+using std::vector;
 
 class level
 {
@@ -12,6 +15,9 @@ private:
 	int _columnCount = 1;
 	int _rowSpacing = 1;
 	int _columnSpacing = 1;
+
+	vector<BrickType> brickTypes;  // level holds reference to every texture needed to build level just once
+	vector < vector<BrickType*> > brickList; //bricks on the level
 
 	Tekstura background = Tekstura();
 };
