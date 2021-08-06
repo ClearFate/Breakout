f5712e609f86b4e162d9798a11f9e9fe3e8fb52c
ClearFate <luckyT320alter@gmail.com>
2021-08-06 23:10:58 +0200

    Added incomplete rotateBounce method

--- DirectionVector.cpp
+++ DirectionVector.cpp
@@ -1,6 +1,9 @@
 #include "DirectionVector.h"
 #include <cmath>
 #include <iostream>
+#include <numbers>
+
+#define PI 3.141592653589793238462643383279502884197169399375105820974944 // it's also in std::numbers::pi in C++20 (here defined will lose precision)
 
 void DirectionVector::perfectBounce(Side pointOfContact)
 {
@@ -25,17 +28,37 @@ double DirectionVector::getVectorLength()
 
 void DirectionVector::rotateBounce(double steepnessInterval, Side direction)
 {
+	// to mirror it back
+	perfectBounce(Side::UP);
+
+	// now we can do calculations to find out mirrored angle
 	double bounceOffAngle;
 	double r = getVectorLength();
+	double x = moveRight;
+	double y = moveUp;
 
-	//TODO finish 
+	// x = r * cos(fi) => fi = acos(x/r) * pi/180
+	double fi = acos(x / r) * PI / 180;   // * PI/180 je da dobijemo stupnjeve a ne radiane
+	double angleDiff;
 
 	if (direction == Side::LEFT) {
 		bounceOffAngle = 175;
+		angleDiff = abs(bounceOffAngle - fi);
 	}
 	else if (direction == Side::RIGHT) {
 		bounceOffAngle = 5;
+		angleDiff = -abs(fi - bounceOffAngle);
 	}
+
+	double resultingAngle = steepnessInterval * angleDiff;
+
+	double newX = x * cos(resultingAngle) - y * sin(resultingAngle);
+	double newY = x * sin(resultingAngle) + y * cos(resultingAngle);
+
+	upDirection = newY;
+	rightDirection = newX;
+	normalize();
+
 }
 
 void DirectionVector::setUpDirection(double upVelocity)
